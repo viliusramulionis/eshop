453d5c1e2a17c8f13b6597058bf6c72c674939c7
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2022-06-20 09:05:30 +0300

    Finished testing

--- controller/products.js
+++ controller/products.js
@@ -42,8 +42,8 @@ router.get('/', async (req, res) => {
     try {
         const products = await Products.find()
         res.status(200).json(products)
-    } catch(error) {
-        res.status(202).send(error)
+    } catch {
+        res.sendStatus(202)
     }
 })
 
